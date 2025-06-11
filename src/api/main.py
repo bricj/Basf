45a75e787d3211080eee5e7af1d95efcda5696a0
bricenojsebastian <bricenojsebastian@javeriana.edu.co>
2025-06-11 02:36:00 -0500

    Preparando despliegue en Render 9

--- src/api/main.py
+++ src/api/main.py
@@ -336,7 +336,7 @@ async def get_data():
             # "total": len(records),
             # "source": "basf_optimized_data",
             # "data": [dict(record) for record in records]
-            dict(record) for record in records
+            [dict(record) for record in records]
         }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
